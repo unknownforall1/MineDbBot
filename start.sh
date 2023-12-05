6b662e8f1c8830bc252f2c86ec02d641ee1cb221
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-12-05 21:07:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Unknownforall1/Auto-filter-bot.git /Auto-filter-bot
+  git clone https://github.com/Unknownforall1/filterbot.git /filterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-filter-bot
+  git clone $UPSTREAM_REPO /filterbot
 fi
-cd /Auto-filter-bot
+cd /filterbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
