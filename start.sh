e6770da14eb51f0f382d625fae3788248e1e602c
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-12-23 14:09:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Unknownforall1/filterbot.git /filterbot
+  git clone https://github.com/Unknownforall1/MineDbBot.git /filterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /filterbot
+  git clone $UPSTREAM_REPO /MineDbBot
 fi
-cd /filterbot
+cd /MineDbBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
