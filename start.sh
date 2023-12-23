99a0be2cf70967bf46408211885696b848cc49b6
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-12-23 14:20:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Unknownforall1/MineDbBot.git /filterbot
+  git clone https://github.com/Unknownforall1/MineDbBot.git /MineDbBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /MineDbBot
